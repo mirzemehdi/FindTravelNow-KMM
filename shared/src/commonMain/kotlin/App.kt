ac0e069fd5d1d14121b48b50e1e5cac84878c9ae
Mirzamehdi <mirzemehdi@gmail.com>
2023-09-30 21:49:45 +0200

    Adding pager indicator

--- shared/src/commonMain/kotlin/App.kt
+++ shared/src/commonMain/kotlin/App.kt
@@ -1,18 +1,30 @@
+import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.pager.HorizontalPager
+import androidx.compose.foundation.pager.PagerState
+import androidx.compose.foundation.pager.rememberPagerState
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import org.jetbrains.compose.resources.ExperimentalResourceApi
@@ -22,7 +34,7 @@ import theme.MyApplicationTheme
 import theme.Yellow_alpha_0
 import theme.Yellow_alpha_39
 
-@OptIn(ExperimentalResourceApi::class)
+@OptIn(ExperimentalResourceApi::class, ExperimentalFoundationApi::class)
 @Composable
 fun App() {
     MyApplicationTheme {
@@ -38,6 +50,18 @@ fun App() {
                 ).padding(40.dp),
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
+
+            val pagerState = rememberPagerState(
+                initialPage = 0,
+                initialPageOffsetFraction = 0f,
+                pageCount = { 3 }
+            )
+            HorizontalPager(state = pagerState) { pageIndex ->
+
+
+            }
+            HorizontalPagerIndicator(pagerState)
+
             Image(
                 painter = painterResource("drawable/onboarding_1.xml"),
                 contentDescription = null,
@@ -51,6 +75,7 @@ fun App() {
                 text = "Trusted and Free",
                 style = MaterialTheme.typography.displayMedium,
                 color = Black,
+                textAlign = TextAlign.Center
             )
 
             Text(
@@ -71,6 +96,29 @@ fun App() {
             }
 
 
+        }
+    }
+}
+
+@OptIn(ExperimentalFoundationApi::class)
+@Composable
+fun HorizontalPagerIndicator(pagerState: PagerState) {
+    Row(
+        Modifier
+            .height(50.dp)
+            .fillMaxWidth(),
+        horizontalArrangement = Arrangement.Center
+    ) {
+        repeat(pagerState.pageCount) { iteration ->
+            val color = if (pagerState.currentPage == iteration) Color.DarkGray else Color.LightGray
+            Box(
+                modifier = Modifier
+                    .padding(2.dp)
+                    .clip(CircleShape)
+                    .background(color)
+                    .size(20.dp)
+
+            )
         }
     }
 }
