2f0d70a8005c8152a16eb9e1be48b7222913deef
Mirzamehdi <mirzemehdi@gmail.com>
2023-10-03 23:25:10 +0200

    Adding sort by

--- shared/src/commonMain/kotlin/ui/screens/top5flights/Top5FlightsScreen.kt
+++ shared/src/commonMain/kotlin/ui/screens/top5flights/Top5FlightsScreen.kt
@@ -1,6 +1,8 @@
 package ui.screens.top5flights
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.ExperimentalLayoutApi
@@ -9,15 +11,26 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.KeyboardArrowDown
+import androidx.compose.material3.Divider
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.SpanStyle
 import androidx.compose.ui.text.buildAnnotatedString
@@ -28,15 +41,19 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import domain.model.FlightInfo
 import ui.components.FlightInfoItem
+import ui.theme.Alabaster
 import ui.theme.Orange_alpha_12
 import ui.theme.strings.Strings
 
 @Composable
 fun Top5FlightsScreen() {
 
+
     val uiState by remember { mutableStateOf(Top5FlightsUiState()) }
 
+
     Column(modifier = Modifier.fillMaxSize()) {
+        SortByDropDown(modifier = Modifier.padding(end = 30.dp, top = 20.dp))
         FLightsListUI(
             modifier = Modifier.weight(1f).padding(horizontal = 30.dp, vertical = 10.dp),
             uiState.flights
@@ -45,6 +62,76 @@ fun Top5FlightsScreen() {
     }
 
 
+}
+
+@Composable
+private fun SortByDropDown(modifier: Modifier) {
+    val shape = RoundedCornerShape(5.dp)
+    var isExpanded by remember { mutableStateOf(false) }
+    Row(
+        modifier = modifier
+            .fillMaxWidth()
+            .wrapContentSize(Alignment.TopEnd)
+            .clip(shape)
+            .border(width = 1.dp, color = Alabaster, shape = shape)
+            .background(color = Color(0xFFFFFFFF), shape = shape)
+            .clickable {
+                isExpanded = !isExpanded
+            }
+            .padding(start = 13.dp, top = 8.dp, bottom = 8.dp, end = 4.dp)
+    ) {
+        Text(
+            modifier = Modifier.padding(end = 44.dp),
+            text = Strings.sort_by,
+            style = MaterialTheme.typography.bodySmall,
+            textAlign = TextAlign.Start,
+            color = Color.Black
+        )
+
+        Icon(
+            imageVector = Icons.Default.KeyboardArrowDown,
+            contentDescription = null,
+            tint = Color.Black
+        )
+
+        DropdownMenu(
+            expanded = isExpanded,
+            onDismissRequest = { isExpanded = false },
+            modifier = Modifier.background(Color.White)
+        ) {
+            DropdownMenuItem(
+                text = {
+                    Text(
+                        modifier = Modifier.padding(end = 44.dp),
+                        text = Strings.price,
+                        style = MaterialTheme.typography.bodySmall,
+                        textAlign = TextAlign.Start,
+                        color = Color.Black
+                    )
+                },
+                onClick = {
+                    isExpanded = false
+                }
+            )
+            Divider(color = Alabaster)
+            DropdownMenuItem(
+                text = {
+                    Text(
+                        modifier = Modifier.padding(end = 44.dp),
+                        text = Strings.date,
+                        style = MaterialTheme.typography.bodySmall,
+                        textAlign = TextAlign.Start,
+                        color = Color.Black
+                    )
+                },
+                onClick = {
+                    isExpanded = false
+                }
+            )
+        }
+
+
+    }
 }
 
 @OptIn(ExperimentalLayoutApi::class)
