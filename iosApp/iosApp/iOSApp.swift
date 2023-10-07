3e197d06406866f10421bba1470d29fd425a1ccd
Mirzamehdi <mirzemehdi@gmail.com>
2023-10-07 22:32:17 +0200

    ios app koin initialization setup

--- iosApp/iosApp/iOSApp.swift
+++ iosApp/iosApp/iOSApp.swift
@@ -1,7 +1,13 @@
 import SwiftUI
+import shared
 
 @main
 struct iOSApp: App {
+    
+    init() {
+        AppInitializer.shared.initialize(isDebug: true, onKoinStart: { _ in })
+    }
+    
 	var body: some Scene {
 		WindowGroup {
 			ContentView()
