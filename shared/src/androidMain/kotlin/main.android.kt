cdb9fa768a4c610a9b09657d89d365ebd4085d80
Mirzamehdi <mirzemehdi@gmail.com>
2023-10-07 22:45:55 +0200

    Android navigation and statusbar color fixing

--- shared/src/androidMain/kotlin/main.android.kt
+++ shared/src/androidMain/kotlin/main.android.kt
@@ -1,5 +1,26 @@
+import android.app.Activity
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.toArgb
+import androidx.compose.ui.platform.LocalView
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowCompat
 import root.App
 
 
-@Composable fun MainView() = App()
+@Composable fun MainView() {
+    val view = LocalView.current
+    if (!view.isInEditMode) {
+        SideEffect {
+            (view.context as Activity).window.run {
+                val systemBarColor = Color.White.toArgb()
+                statusBarColor = systemBarColor
+                navigationBarColor = systemBarColor
+                WindowCompat.getInsetsController(this,view).isAppearanceLightStatusBars=true
+                WindowCompat.getInsetsController(this,view).isAppearanceLightNavigationBars=true
+            }
+        }
+    }
+    App()
+}
