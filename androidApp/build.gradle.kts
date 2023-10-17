038be609cbe182a906dfec7d33fba5ce1f428f6d
Mirzamehdi <mirzemehdi@gmail.com>
2023-10-18 01:25:09 +0200

    Adding Publish to playstore

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -28,6 +28,35 @@ android {
         versionCode = 7
         versionName = "2.0.0"
     }
+
+    buildTypes {
+        val debug by getting {
+            isMinifyEnabled = false
+            isDebuggable = true
+        }
+
+        val release by getting {
+            isMinifyEnabled = true
+            isShrinkResources = true
+            proguardFiles(
+                getDefaultProguardFile("proguard-android.txt"),
+                "proguard-rules.pro"
+            )
+//            signingConfig = signingConfigs.getByName("release")
+        }
+    }
+
+    tasks.register("getAppName") {
+        // Set the task"s actions
+        doLast {
+            val versionName = defaultConfig.versionName
+            val versionCode = defaultConfig.versionCode
+            val appName = "APP-v${versionName}-$versionCode"
+            // Print the app name
+            println(appName)
+        }
+    }
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
@@ -35,7 +64,7 @@ android {
     kotlin {
         jvmToolchain(17)
     }
-    
+
     buildFeatures {
         buildConfig = true
     }
