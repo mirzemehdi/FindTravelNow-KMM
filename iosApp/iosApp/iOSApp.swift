3ba4c8c5a9f35d82338c2b0dd72e62259282af64
Mirzamehdi <mirzemehdi@gmail.com>
2023-11-04 06:45:36 +0100

    Adding Firebase analytics and crashlytics to ios

--- iosApp/iosApp/iOSApp.swift
+++ iosApp/iosApp/iOSApp.swift
@@ -1,12 +1,26 @@
 import SwiftUI
 import shared
+import FirebaseCore
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+
+  func application(_ application: UIApplication,
+
+                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    
+    AppInitializer.shared.initialize(isDebug: true, onKoinStart: { _ in })
+    FirebaseApp.configure()
+      
+    return true
+
+  }
+
+}
+
 
 @main
 struct iOSApp: App {
-    
-    init() {
-        AppInitializer.shared.initialize(isDebug: true, onKoinStart: { _ in })
-    }
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
     
 	var body: some Scene {
 		WindowGroup {
